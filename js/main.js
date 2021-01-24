ef0586f955e5faac6a74f4db5eadf1fdcc97d357
pedrosavio99 <pedro_savio777@hotmail.com>
2021-01-24 10:29:48 -0300

    arrays em js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,19 @@
-var nome= "Pedro Savio";
+/*var nome= "Pedro Savio";
 var idade= 21;
 
-alert(nome + " tem "  + idade + " com esse comando no scritp vc cria poppups em js, da pra criar um arquivo que nem no css3");
+alert(nome + " tem "  + idade + " com esse comando no scritp vc cria poppups em js, da pra criar um arquivo que nem no css3");*/
+
+var frutas = [{nome:"maça" , cor: "vermelha"},{nome:"banana" , cor: "amarelo"}];
+console.log(frutas);
+alert(frutas[1].nome);
+
+var lista = ["maça","pera","uva"];
+lista.push("tijolo");
+lista.push("telha");
+lista.pop();
+console.log(lista);
+
+console.log(lista.toString());
+console.log(lista.join(" pedrosavio "));
+
+
